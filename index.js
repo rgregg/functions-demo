f8f0c5e119e65a43417a18c9e8a9a4acc339af29
Ryan Gregg <rgregg@users.noreply.github.com>
2019-06-20 17:10:49 -0400

    Update index with comments

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+// Using the GoogleCloudPlatform Function Framework for NodeJS
+// See package.json for more details
+
 exports.helloWorld = (req, res) => {
-  res.send('Hello, T-Mobile\n');
+  res.send('Hello, World!\n');
 };
 
